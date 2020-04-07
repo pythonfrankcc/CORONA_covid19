dfc40497e7330237207b1af4af841319c38fb81a
pythonfrankcc <francismurithi475@gmail.com>
2020-04-07 15:06:01 +0300

    added stringent column plus some for the continent

--- covid19CM.py
+++ covid19CM.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[174]:
 
 
 # This Python 3 environment comes with many helpful analytics libraries installed
@@ -22,35 +22,35 @@ for dirname, _, filenames in os.walk('/kaggle/input'):
 # Any results you write to the current directory are saved as output.
 
 
-# In[2]:
+# In[175]:
 
 
 #let us start by reading the data using pandas
-data = pd.read_csv ("../input/covid19/train.csv")
+data = pd.read_csv ("../input/train.csv")
 
 
-# In[3]:
+# In[176]:
 
 
 #lets look at how our data looks like for the first few rows
 data.head()
 
 
-# In[4]:
+# In[177]:
 
 
 #lets look at how our data looks like for the last several rows
 data.tail()
 
 
-# In[5]:
+# In[178]:
 
 
 #lets look at the unique names for the columns and from there also the unique values so as to drop unwanted data
 list(data.columns)
 
 
-# In[6]:
+# In[179]:
 
 
 #dropping the repeated columns which are the last two
@@ -58,14 +58,14 @@ list(data.columns)
 #data = data.drop(repeated_columns_for_dropping,axis = 1)
 
 
-# In[7]:
+# In[180]:
 
 
 #lets look at the train data description to better understand the data
 data.describe()
 
 
-# In[8]:
+# In[181]:
 
 
 print("Number of Territories: ", data['Territory'].nunique())
@@ -74,14 +74,14 @@ print("Dates go from day", max(data['Date']), "to day", min(data['Date']), ", a 
 #print("Countries with Province/State informed: ", data[data['Province/State'].isna()==False]['Country/Region'].unique())
 
 
-# In[9]:
+# In[182]:
 
 
 #let us look at these territories just to make sure that each stands on its own
 print(data['Territory'].unique())
 
 
-# In[10]:
+# In[183]:
 
 
 #from the above data we can see that each country appears only once 
@@ -89,7 +89,7 @@ print(data['Territory'].unique())
 data['Territory'].value_counts()
 
 
-# In[11]:
+# In[184]:
 
 
 '''# produces Pandas Series
@@ -99,7 +99,7 @@ data.groupby('month')[['duration']].sum()
 '''
 
 
-# In[12]:
+# In[185]:
 
 
 #lets check the number of deaths and infected confirmed cases by using plots
@@ -124,7 +124,7 @@ ax2.set_xlabel("Date", size=10)
 # we know that the virus originated from china so we can use this to compare with the China graph for both the confirmed cases against the deaths and check if the graphs flow the same remembering that during some time china changed how it considered whether somebody was considered positive (11/03/2020).This may be registered as a spike and considering other policies that are put in place that may likely affect the number of cases of the infected people.
 # 
 
-# In[13]:
+# In[186]:
 
 
 #lets draw the curve excluding china
@@ -145,7 +145,7 @@ ax2.set_xlabel("Date", size=10)
 
 # Without China we should be getting a smoother curve as which more or less looks like the SIR model for epidemiology where there is a steep rise then a gentle drop in the number of cases but remember that unlike other countries that can learn from China,China had no prior warning of the contagion.
 
-# In[14]:
+# In[187]:
 
 
 confirmed_total_date_China = data[data['Territory']=='China'].groupby(['Date']).agg({'cases':['sum']})
@@ -163,7 +163,7 @@ ax2.set_ylabel("Number of cases", size=10)
 ax2.set_xlabel("Date", size=10)
 
 
-# In[17]:
+# In[188]:
 
 
 confirmed_total_date_kenya = data[data['Territory']=='Kenya'].groupby(['Date']).agg({'cases':['sum']})
@@ -181,7 +181,7 @@ ax2.set_ylabel("Number of cases", size=10)
 ax2.set_xlabel("Date", size=10)
 
 
-# In[27]:
+# In[189]:
 
 
 #looking at the worst hit countries as of now
@@ -226,4 +226,208 @@ total_date_Singapore.plot(ax=plt.gca(), title='Singapore')
 '''
 plt.subplot(2, 2, 4)
 total_date_SouthKorea.plot(ax=plt.gca(), title='SouthKorea')
+
+
+# In[190]:
+
+
+#what type of data can we deduce from the given data
+#we can get the mortality rate in the countries as of the beginning of the beginning and we will compare with other weeks
+data.head()
+
+
+# In[191]:
+
+
+#calculating the difference between cases and target that will help in getting mortality rate
+data['diff']=data['cases'] - data['target']
+
+
+# In[192]:
+
+
+#checking whether the difference column has been created
+data.head()
+
+
+# In[193]:
+
+
+#calculating the mortality rates of different Territories
+data['mortality rate'] = data['diff']/data['cases'] * 100
+
+
+# In[194]:
+
+
+#looking at the data type for the various columns that we have
+print(data.dtypes)
+
+
+# In[195]:
+
+
+print("Number of mortality rates: ", data['mortality rate'].nunique())
+#print("Number of mortality rates: ", data['mortality rate'].unique())
+print("Number of unique differences: ", data['diff'].nunique())
+#print("Number of mortality rates: ", data['diff'].unique())
+
+
+# In[196]:
+
+
+#lets look to see if the mortality rate has been calculated for each country
+data.head()
+
+
+# In[197]:
+
+
+data['mortality rate'] = data['mortality rate'].replace(np.nan, 0.00, regex=True)
+
+
+# In[198]:
+
+
+data.head()
+
+
+# In[199]:
+
+
+#making sure that we have not changed the various data types with the code above
+print(data.dtypes)
+
+
+# In[200]:
+
+
+#pip install pycountry-convert
+#using pycountry-convert in kaggle turned out to be a deliberate scam and it did not work out in my favor
+#used the alternative which is to tweak the dataset in excel manually and added the column for the continent
+
+
+# In[201]:
+
+
+'''#lets group the respective Territories to their Continents this may help in organizing per R0
+import pycountry_convert as pc
+
+country_code = pc.country_name_to_country_alpha2("China", cn_name_format="default")
+print(country_code)
+continent_name = pc.country_alpha2_to_continent_code(country_code)
+print(continent_name)'''
+
+
+# In[202]:
+
+
+'''the next part is to check the modal split of the individual continents but there was lack of data for the various
+continents especially Africa so lets look at sth that we have data on which is which countries have put up stringent
+measures and with those we can use the R0 as a little less than others where people still move freely
+'''
+
+
+# In[203]:
+
+
+#checking whether continent column has any null values
+data['Continent'].isnull()
+
+
+# In[204]:
+
+
+#checking for the data types of all columns
+print(data.dtypes)
+
+
+# In[205]:
+
+
+'''
+for places with high laws on covid we use 3,medium we use 2 and low we use 1 and very low 0
+adapted from https://www.weforum.org/agenda/2020/03/coronavirus-this-is-how-the-world-is-responding/ and
+https://www.vox.com/science-and-health/2020/3/22/21189889/coronavirus-covid-19-pandemic-response-south-korea-phillipines-italy-nicaragua-senegal-hong-kong
+https://www.nation.co.ke/news/How-countries-are-battling-coronavirus/1056-5502012-147wgeoz/index.html
+http://www.xinhuanet.com/english/2020-03/16/c_138883650.htm
+'''
+def label_race (row):
+    if row['Territory'] == 'Republic of Korea (the)' :
+        return 3
+    if row['Territory'] == 'United States of America (the)' :
+        return 2
+    if row['Territory'] == 'Philippines (the)':
+        return 1
+    if row['Territory']  == 'Nicaragua':
+        return 0
+    if row['Territory'] == 'Italy':
+        return 1
+    if row['Territory'] == 'Senegal':
+        return 3
+    if row['Territory'] == 'Singapore':
+        return 3
+    if row['Territory'] == 'Tunisia':
+        return 2
+    if row['Territory'] == 'Kenya':
+        return 2
+    if row['Territory'] == 'France':
+        return 2
+    if row['Territory'] == 'Iran (Islamic Republic of)':
+        return 1
+    if row['Territory'] == 'Germany':
+        return 2
+    if row['Territory'] == 'Switzerland':
+        return 2
+    if row['Territory'] == 'Austria':
+        return 1
+    if row['Territory'] == 'China':
+        return 3
+    if row['Territory'] == 'Japan':
+        return 2
+    if row['Territory'] == 'Saudi Arabia':
+        return 3
+    if row['Territory'] == 'Egypt':
+        return 3
+    if row['Territory'] == 'United Kingdom of Great Britain and Northern Ireland (the)':
+        return 2
+    if row['Territory'] == 'South Africa':
+        return 2
+    if row['Territory'] == 'Uganda':
+        return 1
+    if row['Territory'] == 'Argentina':
+        return 2
+    if row['Territory'] == 'Serbia':
+        return 3
+    if row['Territory'] == 'Czechia':
+        return 2
+    if row['Territory'] == 'Mexico':
+        return 1
+    if row['Territory'] == 'Iraq':
+        return 2
+    if row['Territory'] == 'Astralia':
+        return 2
+    return 0
+#data.apply (lambda row: label_race(row), axis=1)
+data['Stringent'] = data.apply (lambda row: label_race(row), axis=1)
+
+
+# In[206]:
+
+
+data.head()
+
+
+# In[207]:
+
+
+#checking to see if the values for the Stringent stuck
+print("Number of unique values for the Stringent column: ", data['Stringent'].nunique())
+
+
+# In[209]:
+
+
+#checking for the data types of all columns
+print(data.dtypes)
 
